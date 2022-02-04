b9017a7fb8fd0c85c433dcda53a7aac750f1c84b
nextLine <daniel.attitsogbe02@gmail.com>
2022-02-04 21:18:33 +0000

    add some other test cases

--- systems_of_algebraic_equations/main.py
+++ systems_of_algebraic_equations/main.py
@@ -2,21 +2,30 @@ from matrix import Matrix
 from functions import *
 
 # sample data
-arrA = [[3, 2, 3, 4], [4, 4, 3, 2], [1, 4, 4, 3], [2, 3, 1, 1]]
-arrB = [[4], [8], [2], [0]]
+""" arrA = [[3, 2, 3, 4], [4, 4, 3, 2], [1, 4, 4, 3], [4, 5, 6, 7]]
+arrB = [[4], [8], [2], [0]] """
+
+arrA = [[1, -1, 2, -1], [2, -2, 3, -3], [1, 1, 1, 0], [1, -1, 4, 3]]
+arrB = [[-8], [-20], [-2], [4]]
+
+""" arrA = [[1, 0, 1], [-1, 1, 2], [1, -1, 1]]
+arrB = [[2], [2], [1]] """
+ 
+""" arrA = [[1, 2], [3, 4]]
+arrB = [[3], [7]] """
 
 A = Matrix(4, 4)
-A.setDataByArray(arrA)
+A.set_data(arrA)
 A.display()
 
 B = Matrix(4, 1)
-B.setDataByArray(arrB)
+B.set_data(arrB)
 B.display()
 
-gaussResults, finalMatrix = gauss(A, B, True)
-print("GAUSS : ", *gaussResults, sep="    ")
-finalMatrix.display()
+gauss_results, final_matrix = gauss(A, B, True)
+print("GAUSS : ", *gauss_results, sep="    ")
+final_matrix.display()
 
-gaussJordanResults, finalMatrix = gaussJordan(A, B, True)
-print("GAUSS JORDAN : ", *gaussJordanResults, sep="    ")
-finalMatrix.display()
+gauss_jordan_results, final_matrix = gauss_jordan(A, B, True)
+print("GAUSS JORDAN : ", *gauss_jordan_results, sep="    ")
+final_matrix.display()
